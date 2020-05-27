932d517341b1d1c407b05b00fa19076b4399a5ae
senbagaraman04 <senbagaraman04@gmail.com>
2020-05-27 22:15:18 +0530

    Normal List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+
+ artists = [
+    'Artist I - Davido',
+    'Artist II - Wizkid',
+    'Artist III - Burna Boy',
+    'Artist IV - Kiss Daniel',
+    'Artist V - Mayorkun',
+    'Artist VI - Mr. Eazi',
+    'Artist VII - Tiwa Savage',
+    'Artist VIII - Blaqbonez',
+    'Artist IX - Banky W',
+    'Artist X - Yemi Alade',
+    'Artist XI - Perruzi',
+    'Artist XII - Seyi Shay',
+    'Artist XIII - Teni'
+  ];
+
 }
